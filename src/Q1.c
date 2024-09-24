15c9882abd371b0a5680d66a4ff18d338398927b
FrancinaldoSB <francinaldosousabarbosa21@gmail.com>
2024-09-23 22:47:22 -0300

    Refactor and optimize code in Q1.c

--- src/Q1.c
+++ src/Q1.c
@@ -69,6 +69,61 @@ void buscamat(Matricula *m, int codigo, int *enc){
     }
 }
 
+// iii) Cadastrar disciplinas a qualquer momento em uma árvore de disciplinas de um determinado curso, ou
+// seja, um disciplina só pode ser cadastrada se o curso já estiver sido cadastrado, além disso, o período da
+// disciplina deve ser válido, ou seja, estar entre 1 e a quantidade máxima de períodos do curso. A carga
+// horária da disciplina deve ser múltiplo de 15, variando entre 30 e 90. 
+void validar_cargahoraria(int *validar, int cargahoraria){
+    if (cargahoraria % 15 == 0 && (cargahoraria >= 30 && cargahoraria <= 90))
+        *validar = 1;
+}
+
+void validar_periodo(Cursos *curso,int *validar, int periodo){
+    if (curso->qntdperiodos < periodo && periodo >= 1)
+        *validar = 1;
+}
+
+int insere_disc(Disciplina **disc, Disciplina *No){
+    int insere = 0;
+    if (*disc == NULL){
+        *disc = No;
+        No->esq = NULL;
+        No->dir = NULL;
+    }
+    else if (No->cod_disciplina < (*disc)->cod_disciplina)
+            insere_disc(&((*disc)->esq), No);
+    else if (No->cod_disciplina > (*disc)->cod_disciplina)
+            insere_disc(&((*disc)->dir), No);
+    else
+        insere = 1;
+    
+    return insere;
+}
+
+int caddisc(Cursos **curso, Disciplina *No, int idcurso){
+    int validar_h = 0, validar_p = 0, validar_cod = 0;
+    // Função para validar se a carga horaria e múltiplo de 15 é se varia entre 30 e 90.
+    validar_cargahoraria(&validar_h, No->cargah);
+    if(validar_h == 1){
+        if (*curso != NULL){
+            if((*curso)->idcurso == idcurso){
+                // Função para validar se o periodo está no intervalo do periodo do curso.
+                validar_periodo(*curso, &validar_p, No->periodo);
+                if (validar_p == 1)
+                    validar_cod = insere_disc(&((*curso)->disc), No);
+            }
+            else{
+                if(idcurso < (*curso)->idcurso)
+                    caddisc((*curso)->esq, No, idcurso);
+                else
+                    caddisc((*curso)->dir, No, idcurso);
+            }
+        }
+    }
+
+    return validar_cod;
+}
+
 // Essa buscamat vai mudar de lugar, para o main quando este for criado. Isto para otimizar
 void cadnota(Matricula **m, Notas **n, int cod, int semestre, int notafinal){
     int enc;
@@ -153,48 +208,90 @@ void exibir_cursos(Cursos *curso) {
 }
 
 // viii) Mostrar todas as disciplinas de um determinado curso. 
-void exibir_disc_curso(Cursos *curso, int idcurso) {
+void exibir_disc_curso(Disciplina *disc) {
+    if (disc != NULL) {
+        exibir_disc_curso(disc->esq);
+        printf("Código: %d\n", disc->cod_disciplina);
+        printf("Nome: %s\n", disc->nomedisc);
+        printf("Carga horária: %d\n", disc->cargah);
+        printf("Período: %d\n", disc->periodo);
+        printf("\n");
+        exibir_disc_curso(disc->dir);
+    }
+}
+
+void exibir_disc_curso_main(Cursos *curso, int idcurso) {
     if (curso != NULL) {
         if (curso->idcurso == idcurso) {
             Disciplina *disc = curso->disc;
-            while (disc != NULL) {
-                printf("Código: %d\n", disc->cod_disciplina);
-                printf("Nome: %s\n", disc->nomedisc);
-                printf("Carga horária: %d\n", disc->cargah);
-                printf("Período: %d\n", disc->periodo);
-                printf("\n");
-                disc = disc->dir;
-            }
+            exibir_disc_curso(disc);
         }
-        else if (idcurso < curso->idcurso) {
-            exibir_disciplinas(curso->esq, idcurso);
-        }
-        else {
-            exibir_disciplinas(curso->dir, idcurso);
-        }
+        else if (idcurso < curso->idcurso)
+            exibir_disc_curso_main(curso->esq, idcurso);
+        else
+            exibir_disc_curso_main(curso->dir, idcurso);
+       
     }
 }
 
 // ix) Mostrar todas as disciplinas de um determinado período de um curso. 
-void exibir_disc_periodo(Cursos *curso, int idcurso, int periodo){
+void exibir_disc_periodo(Disciplina *disc, int periodo) {
+    if (disc != NULL) {
+        exibir_disc_periodo(disc->esq, periodo);
+        if (disc->periodo == periodo) {
+            printf("Código: %d\n", disc->cod_disciplina);
+            printf("Nome: %s\n", disc->nomedisc);
+            printf("Carga horária: %d\n", disc->cargah);
+            printf("Período: %d\n", disc->periodo);
+            printf("\n");
+        }
+        exibir_disc_periodo(disc->dir, periodo);
+    }
+}
+
+void exibir_disc_periodo_main(Cursos *curso, int idcurso, int periodo){
     if(curso != NULL){
         if(curso->idcurso == idcurso){
-            Disciplina *disc = curso->disc;
-            while(disc != NULL){
-                if(disc->periodo == periodo){
-                    printf("Código: %d\n", disc->cod_disciplina);
-                    printf("Nome: %s\n", disc->nomedisc);
-                    printf("Carga horária: %d\n", disc->cargah);
-                    printf("Período: %d\n", disc->periodo);
-                    printf("\n");
-                }
-            }
+            exibir_disc_periodo(curso->disc, periodo);
         }
-        else if(idcurso < curso->idcurso){
-            exibir_disc_periodo(curso->esq, idcurso, periodo);
-        }
-        else{
-            exibir_disc_periodo(curso->dir, idcurso, periodo);
-        }
+        else if(idcurso < curso->idcurso)
+            exibir_disc_periodo_main(curso->esq, idcurso, periodo);
+        else
+            exibir_disc_periodo_main(curso->dir, idcurso, periodo);
     }
+}
+
+// x) Mostrar todas as disciplinas que um determinado aluno está matriculado 
+void exibir_disc(Cursos *curso, int idcurso) {
+    if (curso != NULL) {
+        if (curso->idcurso == idcurso) {
+            Disciplina *disc = curso->disc;
+            printf("Código: %d\n", disc->cod_disciplina);
+            printf("Nome: %s\n", disc->nomedisc);
+            printf("Carga horária: %d\n", disc->cargah);
+            printf("Período: %d\n", disc->periodo);
+            printf("\n");
+        }
+        else if (idcurso < curso->idcurso)
+            exibir_disc(curso->esq, idcurso);
+        else
+            exibir_disc(curso->dir, idcurso);
+    }
+}
+
+void exibir_disc_aluno(Matricula *mat, Cursos *cursos, int codcurso){
+    if (mat != NULL){
+        exibir_disc(cursos, mat->coddisc);
+        exibir_disc_aluno(mat->esq, cursos, codcurso);
+        exibir_disc_aluno(mat->dir, cursos, codcurso);
+    }
+}
+
+void exibir_disc_aluno_main(Alunos *aluno, Cursos *cursos, int matricula){
+    if(aluno != NULL){
+        if(aluno->matricula == matricula){
+            exibir_disc_matriculadas(aluno->mat, cursos, aluno->codcurso);
+        }
+    } else
+        exibir_disc_aluno(aluno->prox, cursos, matricula);
 }
